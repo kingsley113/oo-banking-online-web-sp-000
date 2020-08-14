186997b333303b949284b6fb0d5927173b8cb9bb
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-08-14 03:41:46 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
     sender.valid? && receiver.valid?
   end
 
-  def execute_transaction(sender, receiver, amount)
+  def execute_transaction
     if @status == "pending" && (sender.balance >= amount)
       sender.withdraw(amount)
       receiver.deposit(amount)
