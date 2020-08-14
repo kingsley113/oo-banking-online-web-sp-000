c7d01e59ff08d6d5d7f3985ceb2712a8bd5bf956
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-08-14 03:47:46 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,12 +16,19 @@ class Transfer
 
   def execute_transaction
     if valid? && @status == "pending" && (sender.balance >= @amount)
-      sender.withdraw(amount)
-      receiver.deposit(amount)
+      sender.withdraw(@amount)
+      receiver.deposit(@amount)
       @status = "complete"
     else
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      receiver.withdraw(@amount)
+      sender.deposit(@amount)
     end
   end
 
