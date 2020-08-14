09f0c744a260a1492061d1d70030802802e09499
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-08-14 03:11:12 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -3,11 +3,16 @@ class BankAccount
   attr_reader :balance
 
   def initialize(name)
-    @name = name.freeze
+    @name = nil
+    set_name(name)
     @balance = 1000
     @status = "open"
   end
 
+  def set_name(name)
+    raise NameChangeError, "Cannot change account holder name." if !@name==nil
+    @name = name
+
 end
 
-# class NameChangeError < TypeError; end
+class NameChangeError < TypeError; end
