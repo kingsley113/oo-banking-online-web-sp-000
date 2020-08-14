51374c9cf0216907b0c1601034196c300c8436cb
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-08-14 03:08:23 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,6 +1,7 @@
 class BankAccount
   attr_accessor :name, :status
   attr_reader :balance
+  
   def initialize(name)
     @name = name.frozen
     @balance = 1000
@@ -9,4 +10,4 @@ class BankAccount
 
 end
 
-class NameChangeError < TypeError; end
+# class NameChangeError < TypeError; end
